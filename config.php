097aa80940adf345f47ceecfd1d13351f803bc60
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-09-12 10:03:26 -0700

    db settings

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config_site_name		= "Site Name";
 
-$db_hostname			= "localhost";
-$db_database			= "egypt";
-$db_username			= "root";
-$db_password			= "1234";
+$db_hostname			= "10.194.111.8";
+$db_database			= "db_d119e59d";
+$db_username			= "user_d119e59d";
+$db_password			= "s,pgOy(S4QHT,;";
 $db_dbdriver			= "mysql";
